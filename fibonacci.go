850639b72db908117f4d48af106f7e26cd10a680
Daniel Schepers <dschepers@enova.com>
2015-11-08 19:12:47 -0600

    done with web server

--- fibonacci.go
+++ fibonacci.go
@@ -2,24 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
-	"os"
+	"net/http"
+	"path"
 	"strconv"
 )
 
 func main() {
-	var err error
-	n, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-	fib_sequence := make([]int, n)
-	for i := 0; i < n; i++ {
-		min_bound := math.Max(float64(i-2), 0.0)
-		max_bound := math.Max(float64(i), 0.0)
-		fib_sequence[i] = nth_fib(i, fib_sequence[int(min_bound):int(max_bound)])
-	}
-	fmt.Println(fib_sequence)
+	fib_server()
 }
 
 func nth_fib(fib_index int, fib_sequence []int) (nth_fib int) {
@@ -31,4 +22,25 @@ func nth_fib(fib_index int, fib_sequence []int) (nth_fib int) {
 	default:
 		return fib_sequence[0] + fib_sequence[1]
 	}
+}
+
+func fib_server() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		url_path, fib_max := path.Split(r.URL.Path[1:])
+		n, err := strconv.Atoi(fib_max)
+		if url_path != "fibonacci/" {
+			panic("format url as fibonacci/:n")
+		}
+		if err != nil {
+			panic(err)
+		}
+		fib_sequence := make([]int, n)
+		for i := 0; i < n; i++ {
+			min_bound := math.Max(float64(i-2), 0.0)
+			max_bound := math.Max(float64(i), 0.0)
+			fib_sequence[i] = nth_fib(i, fib_sequence[int(min_bound):int(max_bound)])
+		}
+		fmt.Fprintln(w, fib_sequence)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
